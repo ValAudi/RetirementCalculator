926a7dc157114f9b4cb724ccae79dea3f9f860ae
VAL <valentineaudi84@gmail.com>
2020-02-11 11:49:20 -0600

    Initial Commit

---
+++ RetirementCalculator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RetirementCalculator
+//
+//  Created by Pamela Audi on 11/02/2020.
+//  Copyright © 2020 Pamela Audi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RetirementCalculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RetirementCalculator
+//
+//  Created by Pamela Audi on 11/02/2020.
+//  Copyright © 2020 Pamela Audi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
